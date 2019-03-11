1ac1ef1d59e7b36d57e32fe957484343ede65e8e
2br-dev <prog@2-br.ru>
2019-03-11 12:42:21 +0300

    db config

--- protected/config/config.db.php
+++ protected/config/config.db.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_BASE', 'admin_cmscelebro');
-define('DB_USER', 'admin_cmscelebro');
-define('DB_PASS', 'f7lSP4l1d9');
+define('DB_BASE', 'burdilo_akvatory');
+define('DB_USER', 'burdilo_akvatory');
+define('DB_PASS', 'F037apple');
 define('DB_PORT', '3306');
 define('DB_PREF', 'db');
