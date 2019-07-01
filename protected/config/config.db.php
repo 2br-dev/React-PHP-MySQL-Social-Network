b22228bb62bfc813f4c0e8fda4735f711af10151
Burdilo <92279@inbox.ru>
2019-07-01 14:35:48 +0300

    01/07/2019 14.35

--- protected/config/config.db.php
+++ protected/config/config.db.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_BASE', 'akvatory');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_BASE', 'burdilo_akvatory');
+define('DB_USER', 'burdilo_akvatory');
+define('DB_PASS', '123456789');
 define('DB_PORT', '3306');
 define('DB_PREF', 'db');
